80e7608553e001ae9a5921bd15fca59a849b42b4
Mihisara <mihisara@hts.asia>
2025-09-23 15:00:00 +0530

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()], 
   server: {
+     host: '0.0.0.0',
     port: 3000,
     proxy: {
       '/api': {
@@ -11,7 +12,7 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
-    allowedHosts: ['localhost', '127.0.0.1', 'mjay.local'],
+    allowedHosts: ['localhost', '127.0.0.1', 'mjay.local','HTSs-Mac-mini.local'],
   },
   preview: {
     port: 4173,
